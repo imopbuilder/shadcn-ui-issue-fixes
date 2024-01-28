5051388415af6a02faef32f923ecd09b287e3cf3
imopbuilder <opbuilder9@gmail.com>
2024-01-28 14:39:28 +0530

    fix: resolve issue 2508

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import { PopoverDemo } from '@/components/pages/client';
+import { AlertDialogDemo } from '@/components/pages/client';
 
 export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
 			<section>
 				<div>
-					<PopoverDemo />
+					<AlertDialogDemo />
+					<div>
+						<p className='text-5xl text-center h-screen'>Hello world 1</p>
+						<p className='text-5xl text-center h-screen'>Hello world 1</p>
+						<p className='text-5xl text-center h-screen'>Hello world 1</p>
+						<p className='text-5xl text-center h-screen'>Hello world 1</p>
+						<p className='text-5xl text-center h-screen'>Hello world 1</p>
+					</div>
 				</div>
 			</section>
 		</main>
--- src/components/pages/client.tsx
+++ src/components/pages/client.tsx
@@ -1,5 +1,16 @@
 'use client';
 
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle, DrawerTrigger } from '@/components/ui/drawer';
@@ -14,6 +25,28 @@ import { Calendar } from '../ui/calendar';
 import { Checkbox } from '../ui/checkbox';
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from '../ui/navigation-menu';
 import { Textarea } from '../ui/textarea';
+
+export function AlertDialogDemo() {
+	return (
+		<AlertDialog>
+			<AlertDialogTrigger asChild>
+				<Button variant='outline'>Show Dialog</Button>
+			</AlertDialogTrigger>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+					<AlertDialogDescription>
+						This action cannot be undone. This will permanently delete your account and remove your data from our servers.
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel>Cancel</AlertDialogCancel>
+					<AlertDialogAction>Continue</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
+	);
+}
 
 export function PopoverDemo() {
 	const [open, setOpen] = useState(false);
