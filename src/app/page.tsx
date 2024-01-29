c3d309b7396afad03320dee026c2b6a9195e7ee8
imopbuilder <opbuilder9@gmail.com>
2024-01-29 10:02:44 +0530

    fix: resolve more shadcn ui issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
-import { AlertDialogDemo } from '@/components/pages/client';
+import { DrawerDemo } from '@/components/pages/client';
 
 export default function Home() {
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
 			<section>
 				<div>
-					<AlertDialogDemo />
-					<div>
-						<p className='text-5xl text-center h-screen'>Hello world 1</p>
-						<p className='text-5xl text-center h-screen'>Hello world 1</p>
-						<p className='text-5xl text-center h-screen'>Hello world 1</p>
-						<p className='text-5xl text-center h-screen'>Hello world 1</p>
-						<p className='text-5xl text-center h-screen'>Hello world 1</p>
-					</div>
+					<DrawerDemo />
 				</div>
 			</section>
 		</main>
--- src/components/pages/client.tsx
+++ src/components/pages/client.tsx
@@ -17,6 +17,7 @@ import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle, Dr
 import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { Label } from '@/components/ui/label';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { DropdownMenuCheckboxItemProps } from '@radix-ui/react-dropdown-menu';
 import { SearchIcon } from 'lucide-react';
 import Link from 'next/link';
@@ -25,6 +26,67 @@ import { Calendar } from '../ui/calendar';
 import { Checkbox } from '../ui/checkbox';
 import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from '../ui/navigation-menu';
 import { Textarea } from '../ui/textarea';
+
+export function DrawerDemo() {
+	return (
+		<Drawer direction='right'>
+			<DrawerTrigger asChild>
+				<Button variant='outline'>Open Drawer</Button>
+			</DrawerTrigger>
+			<DrawerContent className='h-screen top-0 right-0 left-auto mt-0 w-[500px] rounded-none'>
+				<ScrollArea className='h-screen'>
+					<div className='mx-auto w-full p-5'>
+						<DrawerHeader>
+							<DrawerTitle>Theme Color Options</DrawerTitle>
+							<DrawerDescription>
+								* Selected option will be applied to all layout elements (navbar, toolbar, etc.). You can also create your own theme options and color
+								schemes.
+							</DrawerDescription>
+						</DrawerHeader>
+						<div className='p-4 pb-0 space-y-4'>
+							<div className='bg-muted flex items-center justify-center rounded-lg h-32'>
+								<p>Image 1</p>
+							</div>
+							<div className='bg-muted flex items-center justify-center rounded-lg h-32'>
+								<p>Image 2</p>
+							</div>
+							<div className='bg-muted flex items-center justify-center rounded-lg h-32'>
+								<p>Image 3</p>
+							</div>
+							<div className='bg-muted flex items-center justify-center rounded-lg h-32'>
+								<p>Image 4</p>
+							</div>
+							<div className='bg-muted flex items-center justify-center rounded-lg h-32'>
+								<p>Image 4</p>
+							</div>
+							<div className='bg-muted flex items-center justify-center rounded-lg h-32'>
+								<p>Image 5</p>
+							</div>
+							<div className='bg-muted flex items-center justify-center rounded-lg h-32'>
+								<p>Image 6</p>
+							</div>
+							<div className='bg-muted flex items-center justify-center rounded-lg h-32'>
+								<p>Image 7</p>
+							</div>
+							<div className='bg-muted flex items-center justify-center rounded-lg h-32'>
+								<p>Image 8</p>
+							</div>
+							<div className='bg-muted flex items-center justify-center rounded-lg h-32'>
+								<p>Image 9</p>
+							</div>
+							<div className='bg-muted flex items-center justify-center rounded-lg h-32'>
+								<p>Image 10</p>
+							</div>
+							<div className='bg-muted flex items-center justify-center rounded-lg h-32'>
+								<p>Image 11</p>
+							</div>
+						</div>
+					</div>
+				</ScrollArea>
+			</DrawerContent>
+		</Drawer>
+	);
+}
 
 export function AlertDialogDemo() {
 	return (
